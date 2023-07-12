71cec3a200bc4244afe0941ea124f756cba13c31
Omar Richards <omarrichards01@gmail.com>
2023-07-11 23:30:36 -0400

    lab

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Write your solution here!
+ let cats = ["Milo", "Otis", "Garfield"]
+ 
+ function destructivelyAppendCat(name){
+ 
+ name = cats.push("Ralph");
+   
+ }
+ 
+ function destructivelyPrependCat(name){
+    name = cats.unshift("Bob");
+ }
+ 
+ function destructivelyRemoveLastCat(){
+ 
+   cats.pop();
+ }
+ 
+ function destructivelyRemoveFirstCat(){
+ 
+   cats.shift();
+ }
+ 
+ function appendCat(name){
+ 
+   name = [...cats, "Broom"];
+ 
+   return name;
+   
+ }
+ 
+ function prependCat(name){
+ 
+   name = ["Arnold", ...cats]
+   return name;
+ 
+ }
+ 
+ function removeLastCat(){
+ 
+   let lastCat = [...cats]
+   lastCat.pop("Garfield")
+   return lastCat;
+   
+ }
+ 
+ function removeFirstCat(){
+    let firstCat = [...cats]
+  firstCat.shift("Milo")
+   return firstCat;
+ 
+ }
